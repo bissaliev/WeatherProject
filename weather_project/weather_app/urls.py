9a097a0b094ed59a609ad8e941f28076da451151
bissaliev <bissaliev21@gmail.com>
2024-07-18 16:57:43 +0400

    Добавление роутинга в urls.py автокомплита

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -3,4 +3,7 @@ from weather_app import views
 
 app_name = "weather_app"
 
-urlpatterns = [path("", views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("city-autocomplete/", views.city_autocomplete, name="city_autocomplete")
+]
