e8c94fd021bae4723a3a9b1d0834813978516033
bissaliev <bissaliev21@gmail.com>
2024-08-02 19:01:45 +0400

    Изменение шаблонного тега last_cities(теперь работает с сессиями)

--- weather_project/weather_app/templatetags/custom_tags.py
+++ weather_project/weather_app/templatetags/custom_tags.py
@@ -1,32 +1,15 @@
-from django.db.models import Subquery
 from django.template import Library
-from weather_app.models import City, RequestHistory
-from weather_app.utils import get_client_ip
 
 register = Library()
-
-
-@register.inclusion_tag("tags/last_cities.html", takes_context=True)
-def last_cities1(context, count=5):
-    """Получение последних городов, которые запросил пользователь."""
-    request = context.get("request")
-    ip_address = get_client_ip(request)
-    city_ids = RequestHistory.objects.filter(
-        ip_address=ip_address
-    ).values_list("city__id", flat=True)[:count]
-    cities = City.objects.filter(id__in=Subquery(city_ids)).order_by(
-        "-history__created"
-    )
-    return {"cities": cities}
 
 
 @register.inclusion_tag("tags/last_cities.html", takes_context=True)
 def last_cities(context, count=5):
     """Получение последних городов, которые запросил пользователь."""
     request = context.get("request")
-    cities_names = list(request.session.get("previous_cities"))
-    print(cities_names)
-    return {"cities": cities_names}
+    previous_cities = request.session.get("previous_cities")
+    if previous_cities:
+        return {"cities": previous_cities}
 
 
 @register.filter
