23dd98a676f263fb77d4ba18fc4225182516d21a
bissaliev <bissaliev21@gmail.com>
2024-08-08 18:05:02 +0400

    Рефакторинг views

--- weather_project/weather_app/utils.py
+++ weather_project/weather_app/utils.py
@@ -1,36 +1,15 @@
-import requests
 from datetime import datetime
+from typing import Any
+
+import requests
+from requests.exceptions import ConnectionError, HTTPError, Timeout
 
 
-def process_weather_data_of_days(data):
-    """Обработка данных о погоде по дням."""
+class ForecastWeather:
+    """Класс для получения прогноза погоды."""
 
-    hourly = data.get("hourly")
-    times = hourly.get("time")
-    temperatures = hourly.get("temperature_2m")
-    cloud_covers = hourly.get("cloud_cover")
-    weather_by_day = {}
-    for index, time_str in enumerate(times):
-        dt = datetime.fromisoformat(time_str)
-        day = dt.date()
-        if day not in weather_by_day:
-            weather_by_day[day] = {
-                "time": [],
-                "temperature_2m": [],
-                "cloud_cover": []
-            }
-        weather_by_day[day]["time"].append(dt)
-        weather_by_day[day]["temperature_2m"].append(temperatures[index])
-        weather_by_day[day]["cloud_cover"].append(cloud_covers[index])
-    return weather_by_day
-
-
-def get_weather(latitude, longitude, timezone):
-    """Получение данных о погоде."""
-
+    api_url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude": latitude,
-        "longitude": longitude,
         "current": [
             "temperature_2m",
             "relative_humidity_2m",
@@ -38,32 +17,73 @@ def get_weather(latitude, longitude, timezone):
             "precipitation",
             "cloud_cover",
             "wind_speed_10m",
-            "wind_direction_10m"
+            "wind_direction_10m",
         ],
         "hourly": ["temperature_2m", "cloud_cover"],
-        "daily": ["temperature_2m_max", "precipitation_hours", "wind_direction_10m_dominant"],
-        "timezone": timezone,
-        "forecast_days": 7
+        "daily": [
+            "temperature_2m_max",
+            "precipitation_hours",
+            "wind_direction_10m_dominant",
+        ],
+        "forecast_days": 7,
     }
-    api_url = "https://api.open-meteo.com/v1/forecast"
-    response = requests.get(api_url, params=params, timeout=10)
-    if response.status_code == 200:
-        data = response.json()
+
+    def __init__(self, latitude: float, longitude: float, timezone: str):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timezone = timezone
+        self.add_coordinates_to_params()
+
+    def add_coordinates_to_params(self) -> None:
+        """Добавление координат в параметры запроса."""
+        self.params["latitude"] = self.latitude
+        self.params["longitude"] = self.longitude
+        self.params["timezone"] = self.timezone
+
+    def get_weather(self) -> dict[Any, Any]:
+        """Получение прогноза погоды."""
+        data = self.request_weather_forecast()
         current = data.get("current")
         forecast = {
             "current": current,
-            "weather_by_day": process_weather_data_of_days(data)
+            "weather_by_day": self.process_weather_data_of_days(data),
         }
         return forecast
 
+    def request_weather_forecast(self):
+        """Запрос прогноза погоды к OpenMeteo API."""
+        try:
+            response = requests.get(
+                self.api_url, params=self.params, timeout=10
+            )
+            response.raise_for_status()
+            data = response.json()
+            return data
+        except Timeout:
+            print("Вышло время ожидания!")
+        except ConnectionError:
+            print("Произошла ошибка подключения")
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        return None
 
-def get_client_ip(request):
-    """
-    Функция для определения IP-адреса пользователя.
-    """
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-    return (
-        x_forwarded_for.split(",")[-1].strip()
-        if x_forwarded_for
-        else request.META.get("REMOTE_ADDR")
-    )
+    def process_weather_data_of_days(self, data: dict) -> dict:
+        """Обработка данных о погоде по дням."""
+        hourly = data.get("hourly")
+        times = hourly.get("time")
+        temperatures = hourly.get("temperature_2m")
+        cloud_covers = hourly.get("cloud_cover")
+        weather_by_day = {}
+        for index, time_str in enumerate(times):
+            dt = datetime.fromisoformat(time_str)
+            day = dt.date()
+            if day not in weather_by_day:
+                weather_by_day[day] = {
+                    "time": [],
+                    "temperature_2m": [],
+                    "cloud_cover": [],
+                }
+            weather_by_day[day]["time"].append(dt)
+            weather_by_day[day]["temperature_2m"].append(temperatures[index])
+            weather_by_day[day]["cloud_cover"].append(cloud_covers[index])
+        return weather_by_day
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,16 +1,52 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
-from django.core.cache import cache
 from django.db.models import Q
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
 
 from .models import City
-from .utils import get_weather
+from .utils import ForecastWeather
 
 
-class HomeView(TemplateView):
-    """Главная страница."""
+class CityAddToSessionMixin:
+    """Миксин для добавление названия запрошенного города в сессии."""
+
+    def add_city_to_session(self, city_name: str) -> None:
+        """
+        Добавление названия запрошенного города в сессии для дальнейшего
+        отображения пользователю.
+        """
+        previous_cities = list(
+            dict.fromkeys(
+                [city_name] + self.request.session.get("previous_cities", [])
+            )
+        )
+        while previous_cities and len(previous_cities) > 10:
+            previous_cities.pop()
+        self.request.session["previous_cities"] = previous_cities
+
+
+class ForecastMixin:
+    """
+    Миксин для формирования параметров и вызова функции для получения
+    прогноза погоды.
+    """
+
+    def get_forecast(self, city: City) -> Dict[str, Any]:
+        """
+        Формирование параметров и вызов функции для получения прогноза погоды.
+        """
+        forecast_class = ForecastWeather(
+            city.latitude, city.longitude, city.timezone
+        )
+        forecast = forecast_class.get_weather()
+        forecast |= {"name": city.ru_name}
+        return forecast
+
+
+class HomeView(ForecastMixin, CityAddToSessionMixin, TemplateView):
+    """Класс представления для получения прогноза погоды."""
 
     template_name = "weather_app/home.html"
     extra_context = {"title": "Погода"}
@@ -19,46 +55,10 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         city_name = self.request.POST.get("city")
         if city_name:
-            city = self.get_city(city_name)
-            if not city:
-                context["error_msg"] = (
-                    f"Город {city_name} не найдет в базе данных!"
-                )
-            else:
-                forecast = self.get_forecast(city)
-                context["forecast"] = forecast
+            city = get_object_or_404(City, ru_name=city_name)
+            context["forecast"] = self.get_forecast(city)
+            self.add_city_to_session(city_name)
         return context
-
-    def get_city(self, city_name: str) -> Optional[City]:
-        """
-        Получаем город из базы данных или из кеша, обновляющийся каждые 15 минут.
-        """
-
-        cache_key = f"city_{city_name}"
-        city = cache.get(cache_key)
-        if not city:
-            try:
-                city = City.objects.get(ru_name=city_name)
-            except City.DoesNotExist:
-                return None
-            cache.set(cache_key, city, 15 * 60)  # 15 минут
-        return city
-
-    def get_forecast(self, city: City) -> Dict[str, Any]:
-        """
-        Получаем прогноз погоды по городу из сервера погоды или из кеша,
-        обновляющийся каждые 15 минут.
-        """
-
-        cache_key = f"forecast_{city.ru_name}"
-        forecast = cache.get(cache_key)
-        if not forecast:
-            forecast = get_weather(
-                city.latitude, city.longitude, city.timezone
-            )
-            forecast |= {"name": city.ru_name}
-            cache.set(cache_key, forecast, 15 * 60)  # 15 минут
-        return forecast
 
     def post(self, request, *args, **kwargs):
         return self.get(request, *args, **kwargs)
@@ -74,3 +74,7 @@ def city_autocomplete(request) -> JsonResponse:
         )
         cities = list(qs.values("ru_name", "en_name"))
         return JsonResponse(cities, safe=False)
+
+
+def page_not_found(request, exception):
+    return render(request, "weather_app/404.html", status=404)
