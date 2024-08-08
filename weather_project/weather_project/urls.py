d94e50964ebd38a7f8ca084af22bb80bdaa3b0de
bissaliev <bissaliev21@gmail.com>
2024-08-08 15:26:45 +0400

    Добавление страницы ошибки 404

--- weather_project/weather_project/urls.py
+++ weather_project/weather_project/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
+handler404 = "weather_app.views.page_not_found"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +11,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
